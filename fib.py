ba6655dbefe73eec6c4d564cc30bf4c5700d7088
Abdulla Adel Aljaberi <abdulla.aljaberi@tii.ae>
2024-04-18 10:19:02 +0200

    Added fib functionality using a while loop V1

--- fib.py
+++ fib.py
@@ -1,4 +1,16 @@
 ##!/usr/bin/env python
 
-nterms = int(input('How many terms'))
-print('1 1 2 3 5 8 11')
+nterms = int(input('How many terms?\n'))
+
+n1, n2 = 0 , 1
+
+count = 0
+
+print('fib sequence: ')
+while count < nterms:
+    print(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    
+    count += 1
